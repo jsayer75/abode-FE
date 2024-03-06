92274de6c91d62d9bdb00d04be282685e66b28a3
James Sayer <james.sayer760@gmail.com>
2024-03-06 08:43:53 -0500

    Fixed auto complete UX issue

--- src/components/attendeesAutoComplete/AttendeesAutoComplete.tsx
+++ src/components/attendeesAutoComplete/AttendeesAutoComplete.tsx
@@ -1,36 +1,50 @@
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
 import CircularProgress from "@mui/material/CircularProgress";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Attendee } from "../eventCard/eventCard.interface";
 import debounce from "lodash/debounce";
 import { apiService } from "../../utils/api";
 
 interface Props {
-    onChangeAttendeesSelection: (attendees: Attendee[]) => void
-    defaultValue: Attendee[]
+  onChangeAttendeesSelection: (attendees: Attendee[]) => void;
+  defaultValue: Attendee[];
 }
-export default function AttendeesAutoComplete({onChangeAttendeesSelection, defaultValue = []}: Props) {
+export default function AttendeesAutoComplete({
+  onChangeAttendeesSelection,
+  defaultValue = [],
+}: Props) {
   const [open, setOpen] = useState(false);
+  const [textValue, setTextValue] = useState("");
+  const refInput = useRef<null | HTMLInputElement>(null);
   const [options, setOptions] = useState<Attendee[]>([]);
   const [value, setValues] = useState<Attendee[]>(defaultValue);
-  const loading = open && options.length === 0;
-
+  const [loading, setLoading] = useState(false);
 
   const debouncedSearch = debounce(async (value: string) => {
-
-    if(!value) {
-        setOptions([])
+    if (!value) {
+      setOptions([]);
     }
-    const suggestedAttendees = await apiService.get<Attendee[]>("user/attendees", {
-      search: value,
-    });
-    setOptions(suggestedAttendees)
+    const suggestedAttendees = await apiService.get<Attendee[]>(
+      "user/attendees",
+      {
+        search: value,
+      }
+    );
+    setOptions(suggestedAttendees);
 
-
+    setLoading(false);
   }, 500);
 
   const changeHandler = (value: string) => {
+    setLoading(true);
+    setTextValue(value);
+    if (!value) {
+      setOptions([]);
+      setLoading(false);
+
+      return;
+    }
     debouncedSearch(value);
   };
 
@@ -38,22 +52,27 @@ export default function AttendeesAutoComplete({onChangeAttendeesSelection, defau
     if (!open) {
       setOptions([]);
     }
+    return () => setTextValue("");
   }, [open]);
 
-//   useEffect(() => {
-//     debugger
-//     setValues(defaultValue)
-//   },[defaultValue])
+  const getLoadingText = () => {
+    if (!textValue.length) {
+      return "Please write something to search";
+    } else if (textValue.length && loading) {
+      return "Loading...";
+    }
+    return "No Option";
+  };
 
   return (
     <Autocomplete
       multiple
       open={open}
       value={value}
-
       onChange={(_event, value) => {
-        setValues(value)
-        onChangeAttendeesSelection(value)}}
+        setValues(value);
+        onChangeAttendeesSelection(value);
+      }}
       onOpen={() => {
         setOpen(true);
       }}
@@ -64,11 +83,15 @@ export default function AttendeesAutoComplete({onChangeAttendeesSelection, defau
       getOptionLabel={(option) => option.email}
       options={options}
       loading={loading}
+      noOptionsText={getLoadingText()}
+      loadingText={getLoadingText()}
+      ref={refInput}
       renderInput={(params) => (
         <TextField
           {...params}
           label="Select Attendees"
           onChange={(e) => changeHandler(e.target.value)}
+          // ref={refInput}
           InputProps={{
             ...params.InputProps,
             endAdornment: (
